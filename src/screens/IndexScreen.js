06abc294a82c2d623e38b5b91a7b43da26aaa7f1
LayMui <mui_ume@hotmail.com>
2021-04-02 19:56:38 +0800

    Add reducer

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,21 +1,25 @@
-import React , { useContext} from 'react';
-import { View, Text, StyleSheet, FlatList } from 'react-native';
+import React, { useContext } from 'react';
+import { View, Text, StyleSheet, FlatList, Button } from 'react-native';
 import BlogContext from '../context/BlogContext';
 
 const IndexScreen = () => {
-    const blogPosts = useContext(BlogContext);
-    return (
-        <View>
-           <FlatList
-               data={blogPosts}
-               keyExtractor={(blogPost) => blogPost.title}
-               renderItem={({item}) => {
-                    return <Text>{item.title}</Text>
-               }}
-           />
-        </View>
-    );
+  const { data, addBlogPost } = useContext(BlogContext);
+
+  return (
+    <View>
+      <Text>Index Screen</Text>
+      <Button title="Add Post" onPress={addBlogPost} />
+      <FlatList
+        data={data}
+        keyExtractor={blogPost => blogPost.title}
+        renderItem={({ item }) => {
+          return <Text>{item.title}</Text>;
+        }}
+      />
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({});
+
 export default IndexScreen;
