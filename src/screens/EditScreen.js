803521aa7ad2da4cd671ceb15f09bd95b1eb1a4c
LayMui <mui_ume@hotmail.com>
2021-04-20 19:44:53 +0800

    Wrap up

--- src/screens/EditScreen.js
+++ src/screens/EditScreen.js
@@ -4,18 +4,21 @@ import { Context } from '../context/BlogContext';
 import BlogPostForm from '../components/BlogPostForm';
 
 const EditScreen = ({ navigation }) => {
-  const { state } = useContext(Context);
+  const id = navigation.getParam('id');
+  const { state, editBlogPost } = useContext(Context);
 
-  const blogPost = state.find(
-    blogPost => blogPost.id === navigation.getParam('id')
+  const blogPost = state.find(blogPost => blogPost.id === id);
+
+  return (
+    <BlogPostForm
+      initialValues={{ title: blogPost.title, content: blogPost.content }}
+      onSubmit={(title, content) => {
+        editBlogPost(id, title, content, () => navigation.pop());
+      }}
+    />
   );
-
-   return <BlogPostForm/>
-  
 };
 
-const styles = StyleSheet.create({
-    
-});
+const styles = StyleSheet.create({});
 
 export default EditScreen;
